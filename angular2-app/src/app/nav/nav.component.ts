fb05d72dcff7c5fff188a917eae307c3318817ae
amasenov <amasenov@outlook.com>
2017-06-18 14:52:07 +0200

    v. 3.1.1

--- angular2-app/src/app/nav/nav.component.ts
+++ angular2-app/src/app/nav/nav.component.ts
@@ -8,17 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   showUserMenu: Boolean = false;
   showModal: Boolean = false;
-  modalStyle: String = 'none';
 
   constructor() { }
   
   openModal(){
     if(this.showModal){
       this.showModal = false;
-      this.modalStyle = 'none';
     } else{
       this.showModal = true;
-      this.modalStyle = 'block';
     }
   }
 
