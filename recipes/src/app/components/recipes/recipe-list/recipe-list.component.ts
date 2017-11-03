aba884c32dbcaf194142fbf3fdb08317de5b0a6a
amasenov <amasenov@outlook.com>
2017-11-03 04:54:52 +0100

    recipe v.1.1
    - Updated Angular to version 5.0.0
    - Modified components

--- recipes/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ recipes/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.module';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'http://lorempixel.com/50/50/technics'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'http://lorempixel.com/50/50/technics')
+  ];
 
   constructor() { }
 
