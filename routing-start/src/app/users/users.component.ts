a927f175432f3c2324529d6269c1fb82f275b96a
amasenov <amasenov@outlook.com>
2017-11-21 02:59:52 +0100

    routing
    Project to practice routing

---
+++ routing-start/src/app/users/users.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent {
+  users = [
+    {
+      id: 1,
+      name: 'Aleks'
+    },
+    {
+      id: 2,
+      name: 'Stefan'
+    },
+    {
+      id: 3,
+      name: 'Ivan'
+    }
+  ];
+}
